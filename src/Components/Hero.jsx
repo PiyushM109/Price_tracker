7e65b306688538de47ef3473b5e001ae2736ece1
Piyush <piyushmore109@gmail.com>
2024-02-11 17:03:57 +0530

    Fetching data

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,27 +1,112 @@
 import { Typography } from "@mui/material";
 import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 import HeroCarousel from "./HeroCarousel";
-
+import TextField from "@mui/material/TextField";
+import { Button } from "@mui/material";
+import { useState } from "react";
+import scrapeAndStoreProduct from "../../lib/actions";
 
 const Hero = () => {
+  const [searchProduct, setSearchProduct] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const isValidAmazonUrl = (link) => {
+    try {
+      const parsedUrl = new URL(link);
+      const hostname = parsedUrl.hostname;
+      if (
+        hostname.includes("amazon.com") ||
+        hostname.includes("amazon.in") ||
+        hostname.includes(".amazon.")
+      ) {
+        console.log(hostname);
+        return true;
+      }
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+    return false;
+  };
+  const handleSubmit = async () => {
+    const isValid = isValidAmazonUrl(searchProduct);
+    if (!isValid) {
+      setSearchProduct("");
+      return alert("Please provide valid product link");
+    }
+    try {
+      setIsLoading(true);
+      const product = await scrapeAndStoreProduct(searchProduct);
+    } catch (e) {
+      console.log(e);
+    } finally {
+
+      setSearchProduct("");
+    }
+  };
   return (
-    <div style={{ margin: "5rem",display: "flex", justifyContent:"space-between", flexWrap:"wrap" }}>
-     <div>
-     <div style={{ display: "flex", flexWrap: "nowrap", color: "" }}>
-        <Typography>Track price of your favorite product</Typography>
-        <ArrowForwardIcon />
-      </div>
-      <div style={{ display: "flex", flexWrap: "wrap", color: "" }}>
-        <Typography mt={2} style={{fontSize:"1.5rem"}}>
-          <h1>Unleash the power of<br></br>
-            <span style={{color:"#616161", fontFamily:"bold",fontSize:"3rem"}}> Web Scrapping </span> and <span style={{color:"#616161", fontFamily:"bold",fontSize:"3rem"}}>React</span>
-          </h1>
-        </Typography>
-      </div>
+    <div
+      style={{
+        marginTop: "3rem",
+        marginLeft: "1rem",
+        marginRight: "1rem",
+        display: "flex",
+        justifyContent: "space-evenly",
+        flexWrap: "wrap",
+      }}
+    >
       <div>
-        
+        <div style={{ display: "flex", flexWrap: "nowrap", color: "" }}>
+          <Typography>Track price of your favorite product</Typography>
+          <ArrowForwardIcon />
+        </div>
+        <div style={{ display: "flex", flexWrap: "wrap", color: "" }}>
+          <Typography mt={2} style={{ fontSize: "1.5rem" }}>
+            <h1>
+              Unleash the power of<br></br>
+              <span
+                style={{
+                  color: "#616161",
+                  fontFamily: "bold",
+                  fontSize: "3rem",
+                }}
+              >
+                {" "}
+                Web Scrapping{" "}
+              </span>{" "}
+              and{" "}
+              <span
+                style={{
+                  color: "#616161",
+                  fontFamily: "bold",
+                  fontSize: "3rem",
+                }}
+              >
+                React
+              </span>
+            </h1>
+          </Typography>
+        </div>
+        <div
+          style={{ display: "flex", flexWrap: "nowrap", marginBottom: "2rem" }}
+        >
+          <TextField
+            fullWidth
+            value={searchProduct}
+            onChange={(e) => setSearchProduct(e.target.value)}
+            id="outlined-basic"
+            label="Product-link"
+            size="small"
+          />
+          <Button
+            variant="contained"
+            style={{ backgroundColor: "#616161", marginLeft: "4px" }}
+            disabled ={searchProduct===""}
+            onClick={handleSubmit}
+          >
+            {isLoading ? "Searching" : "Search"}
+          </Button>
+        </div>
       </div>
-     </div>
       <HeroCarousel />
     </div>
   );
--- src/Components/HeroCarousel.jsx
+++ src/Components/HeroCarousel.jsx
@@ -1,20 +1,38 @@
-import "react-responsive-carousel/lib/styles/carousel.min.css"; 
-import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
 
 function HeroCarousel() {
   return (
-    <div style={{maxWidth:"500px", borderRadius:"30px"}}><Carousel showThumbs={false} autoPlay infiniteLoop interval={2500} showArrows={false} showStatus={false}>
-    <div>
-        <img  style={{ height:"600px", borderRadius:"30px"}} src="https://images.unsplash.com/photo-1567538096630-e0c55bd6374c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Y2hhaXJ8ZW58MHx8MHx8fDA%3D" />
+    <div style={{ maxWidth: "500px", borderRadius: "30px" }}>
+      <Carousel
+        showThumbs={false}
+        // autoPlay
+        // infiniteLoop
+        // interval={2500}
+        showArrows={false}
+        showStatus={false}
+      >
+        <div>
+          <img
+            style={{ height: "600px", borderRadius: "30px" }}
+            src="https://images.unsplash.com/photo-1567538096630-e0c55bd6374c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Y2hhaXJ8ZW58MHx8MHx8fDA%3D"
+          />
+        </div>
+        <div>
+          <img
+            style={{ height: "600px", borderRadius: "30px" }}
+            src="https://images.unsplash.com/photo-1641154748135-8032a61a3f80?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGJvb2slMjBjb3ZlcnxlbnwwfHwwfHx8MA%3D%3D"
+          />
+        </div>
+        <div>
+          <img
+            style={{ height: "600px", borderRadius: "30px" }}
+            src="https://images.unsplash.com/photo-1544117519-31a4b719223d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8c21hcnQlMjB3YXRjaHxlbnwwfHwwfHx8MA%3D%3D"
+          />
+        </div>
+      </Carousel>
     </div>
-    <div>
-        <img style={{height:"600px",borderRadius:"30px"}} src="https://images.unsplash.com/photo-1641154748135-8032a61a3f80?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGJvb2slMjBjb3ZlcnxlbnwwfHwwfHx8MA%3D%3D" />
-    </div>
-    <div>
-        <img style={{height:"600px",borderRadius:"30px"}} src="https://images.unsplash.com/photo-1544117519-31a4b719223d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8c21hcnQlMjB3YXRjaHxlbnwwfHwwfHx8MA%3D%3D" />
-    </div>
-</Carousel></div>
-  )
+  );
 }
 
-export default HeroCarousel
+export default HeroCarousel;
